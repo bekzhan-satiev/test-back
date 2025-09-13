d08830edcfa5f1f5129077ead4556544c8143660
Bekzhan Satiev <beksdeveloper@gmail.com>
2025-09-13 15:31:51 +0600

    Return headers

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class TestController {
     @GetMapping("/api/test")
-    public String test(){
-        return "test";
+    public Map<String, String> test(@RequestHeader Map<String, String> headers) {
+        return headers;
     }
 }
