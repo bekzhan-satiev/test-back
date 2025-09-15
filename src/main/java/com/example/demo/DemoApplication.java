35e5d976d1262e91ac64e520a4ade918be6ceb09
Бекжан Сатиев <bsatiev@srs.lan>
2025-09-15 13:06:18 +0000

    A

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import kg.gov.ukuk.requestlogger.service.RequestLoggerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,18 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-        // Start Spring Boot and get the context
-        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-
-        // Try to get the RequestLoggerService bean
-        if (context.containsBean("requestLoggerService")) {
-            RequestLoggerService loggerService = context.getBean(RequestLoggerService.class);
-            System.out.println("RequestLoggerService bean found: " + loggerService);
-
-        } else {
-            System.out.println("RequestLoggerService bean NOT found!");
-        }
-
+        SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
